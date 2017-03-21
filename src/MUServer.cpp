e67c3323f9e891564145d6a9f783cf17a475b8d0
zhomgwu <403547081@qq.com>
2017-03-21 16:34:47 +0800

    same code

---
+++ src/MUServer.cpp
@@ -0,0 +1,25 @@
+#include "MUServer.h"
+
+MUServer::MUServer() {
+}
+
+MUServer::~MUServer() {
+}
+
+bool MUServer::initServer() {
+	return true;
+}
+
+bool MUServer::startServer() {
+	return true;
+}
+
+bool MUServer::exitServer() {
+	return true;
+}
+
+int main(int argc, char **argv) {
+	
+
+	return 0;
+}
